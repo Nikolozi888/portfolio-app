12a5716e960e4b0323c1e2c991c0177cbaae17f8
Nikolozi888 <melashvilinikolozi@gmail.com>
2025-12-16 23:47:50 +0400

    Observer For About Model

--- app/Models/About.php
+++ app/Models/About.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Observers\AboutObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+#[ObservedBy([AboutObserver::class])]
 class About extends Model
 {
-
     use HasFactory;
     protected $guarded = [];
 
